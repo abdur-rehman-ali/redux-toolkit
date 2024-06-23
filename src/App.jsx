7e525e1a10d11d198b52ba32fd4578281be1da38
abdur-rehman-ali <alijoyia911@gmail.com>
2024-06-23 07:23:06 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Redux toolkit</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit dolorem ad culpa tenetur vitae numquam necessitatibus reprehenderit molestiae ducimus consectetur.</p>
+    </div>
+  )
+}
+
+export default App
